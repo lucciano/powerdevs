5d34e84f8edf6efdd895ae4f1ea7d01d61067aec
fbergero2 <fbergero2@005b60d0-fea2-4bd5-a63e-298cc19d6454>
2011-07-18 19:42:07 +0000

    Asking for re-writing
    git-svn-id: svn://svn.code.sf.net/p/powerdevs/code/trunk@647 005b60d0-fea2-4bd5-a63e-298cc19d6454

--- bin/run.sh
+++ bin/run.sh
@@ -1,9 +1,30 @@
 #!/bin/bash
 if [ ! -d $HOME/powerdevs ]
 then
+  # No previous instalation and first run
 	echo "Installing PowerDEVS..."
 	cp -a /opt/powerdevs $HOME/powerdevs
 	rm $HOME/powerdevs/bin/run.sh
+  cd $HOME/powerdevs/bin
+  ./pdme
+else
+  VEROPT=`cat /opt/powerdevs/version`
+  VERHOME=`cat $HOME/powerdevs/version`
+  if [  "$VEROPT" == "$VERHOME" ];
+  then
+    # Previous instalation and same version
+    cd $HOME/powerdevs/bin
+    ./pdme
+  else
+    # Previous instalation and different version
+    OW=`zenity --question --text "There is a different version of PowerDEVS on your home folder. Do you wish to overwrite it?"; echo $?`
+    if [ $OW == 0 ];
+    then 
+    	rm -rf $HOME/powerdevs/
+	    cp -a -f /opt/powerdevs $HOME/powerdevs
+    	rm $HOME/powerdevs/bin/run.sh
+      cd $HOME/powerdevs/bin
+      ./pdme
+    fi
+  fi
 fi
-	cd $HOME/powerdevs/bin
-	./pdme
