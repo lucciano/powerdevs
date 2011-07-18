eba9c19fd88e013a968ea8e404725a61de1bbaec
fbergero2 <fbergero2@005b60d0-fea2-4bd5-a63e-298cc19d6454>
2011-07-18 20:47:19 +0000

    Now the build deb scripts writes the version to the deb
    git-svn-id: svn://svn.code.sf.net/p/powerdevs/code/trunk@652 005b60d0-fea2-4bd5-a63e-298cc19d6454

--- build_deb.sh
+++ build_deb.sh
@@ -4,6 +4,7 @@ echo "Retrieving latest from SVN";
 svn up
 svnversion >rev
 cat version.major rev > version
+VER=`cat version`
 echo "Building PowerDEVS DEB package for $ARCH";
 echo "Building Binaries";
 make -f Makefile clean
@@ -13,10 +14,14 @@ svn export deb tmp_deb
 mkdir ./tmp_deb/opt/powerdevs
 svn export bin ./tmp_deb/opt/powerdevs/bin
 if [ "$ARCH" == "i686" ]; then
-  rm ./tmp_deb/DEBIAN/control.amd64; mv ./tmp_deb/DEBIAN/control.i386 ./tmp_deb/DEBIAN/control
+  cat ./tmp_deb/DEBIAN/control.i386 | awk -v VERSION="$VER" '{ if(index($0,"Version: ")>=1) print "Version: " VERSION ; else print $0;}' >  ./tmp_deb/DEBIAN/control
+  rm ./tmp_deb/DEBIAN/control.amd64; 
+  rm ./tmp_deb/DEBIAN/control.i386; 
 fi
 if [ "$ARCH" == "x86_64" ]; then
-  rm ./tmp_deb/DEBIAN/control.i386; mv ./tmp_deb/DEBIAN/control.amd64 ./tmp_deb/DEBIAN/control
+  cat ./tmp_deb/DEBIAN/control.amd64 | awk -v VERSION="$VER" '{ if(index($0,"Version: ")>=1) print "Version: " VERSION ; else print $0;}' >  ./tmp_deb/DEBIAN/control
+  rm ./tmp_deb/DEBIAN/control.i386; 
+  rm ./tmp_deb/DEBIAN/control.i386; 
 fi
 cp ./bin/pd* ./tmp_deb/opt/powerdevs/bin # copy the binaries
 cp ./bin/original.ini ./tmp_deb/opt/powerdevs/bin/powerdevs.ini
@@ -30,3 +35,5 @@ svn export examples ./tmp_deb/opt/powerdevs/examples
 svn export atomics ./tmp_deb/opt/powerdevs/atomics
 svn export output ./tmp_deb/opt/powerdevs/output
 dpkg -b tmp_deb powerdevs.deb
+rm rev
+rm version
