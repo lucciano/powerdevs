d74e913420d7e6fb68a0e2e4fcda07b418a3a3e9
fbergero2 <fbergero2@005b60d0-fea2-4bd5-a63e-298cc19d6454>
2013-03-15 14:12:37 +0000

    git-svn-id: svn://svn.code.sf.net/p/powerdevs/code/trunk@895 005b60d0-fea2-4bd5-a63e-298cc19d6454

--- src/pdme_v2/graphics/library_window.cpp
+++ src/pdme_v2/graphics/library_window.cpp
@@ -22,33 +22,34 @@
 ****************************************************************************/
 
 #include <QToolBox>
+#include <QDebug>
+#include <QScrollBar>
 #include <QGraphicsView>
 
 #include <library_window.h>
 #include <library_scene.h>
 #include <globals.h>
+#include <math.h>
 
-LibraryWindow::LibraryWindow(): QDockWidget()
-{
+LibraryWindow::LibraryWindow(): QDockWidget() {
 	setFeatures(DockWidgetMovable | DockWidgetFloatable);
 }
-LibraryWindow::~LibraryWindow()
-{
+
+LibraryWindow::~LibraryWindow() {
 }
-QSize LibraryWindow::sizeHint() const
-{
+
+QSize LibraryWindow::sizeHint() const {
   int width = getSetting("LibraryWidth").toInt();
   return QSize(width,minimumHeight());
 }
-void LibraryWindow::resizeEvent(QResizeEvent * e)
-{
+
+void LibraryWindow::resizeEvent(QResizeEvent * e) {
   QToolBox *tb = dynamic_cast<QToolBox*>(widget());
   for (int j=0;j<tb->count();j++)
   {
     QGraphicsView *view = (QGraphicsView*)tb->widget(j);
     LibraryScene *ls = (LibraryScene*)view->scene();
-    if (!ls)
-      continue;
+    Q_ASSERT(ls!=NULL);
     double max_width=1,max_height=1;
     for (int i=0;i<ls->blocks.size();i++)
     {
@@ -61,16 +62,24 @@ void LibraryWindow::resizeEvent(QResizeEvent * e)
     int columns = tb->width()/max_width;
     columns = (columns < 1 ? 1 : columns);
     int col=0,row=0;
+    float minx=1e10,miny=1e10,maxx=0,maxy=0;
     for (int i=0;i<ls->blocks.size();i++) {
       ls->blocks.at(i)->setPos(col*max_width,row*max_height);
+      float width=ls->blocks.at(i)->getBoundingHeight()/2;
+      if (col*max_width<minx) minx=col*max_width;
+      if (col*max_width>maxx) maxx=col*max_width;
+      if (row*max_height-width<miny) miny=row*max_height-width;
+      if (row*max_height+width>maxy) maxy=row*max_height+width;
       col++;
       if (col==columns) {
         row++;
         col=0;
       }
     }
-    view->scene()->setSceneRect(-max_width/2-10,-max_height/2-10,tb->width(),tb->height());
+    view->scene()->setSceneRect(QRectF(minx,miny,fabs(minx-maxx),fabs(miny-maxy)));
+    //qDebug() << "Setting scene size to " << -max_width/2-10 << " " << -max_height/2-10 << " " << tb->width() << " " <<tb->height();
     view->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
-    view->resize(30,30);
+    view->setSceneRect(QRectF(minx,miny,fabs(minx-maxx),fabs(miny-maxy)));
+    view->verticalScrollBar()->setValue(view->verticalScrollBar()->minimum());
   }
 }
